5407e9064b9632b2c2f57cb94bde62e77b34e9f5
Sowmya Parthiban <sparthi1@jh.edu>
2022-07-18 19:33:43 -0400

    update memory req
    comment out trial foreach loop
    
    normalize counts into logcounts
    
    try pTau instead of pAbeta

--- code/01_correlation_exploration/01_calculate_correlation.R
+++ code/01_correlation_exploration/01_calculate_correlation.R
@@ -4,7 +4,7 @@
 #             create_shell = TRUE,
 #             queue = "bluejay",
 #             cores = 15L,
-#             memory = "20G",
+#             memory = "10G",
 #             command = "Rscript 01_calculate_correlation.R",
 #             create_logdir = TRUE
 #         )
@@ -14,10 +14,12 @@ library(GenomicRanges)
 library(BiocParallel)
 library(doParallel)
 library(here)
-library(sessionInfo)
+library(sessioninfo)
 #BiocManager::install("gpuMagic")
 
-numCores <- makeCluster(detectCores(), type='PSOCK') # grabs max available
+#numCores <- makeCluster(detectCores(), type='PSOCK') # grabs max available
+#switch to 4 for now
+numCores <- 4
 
 options('mc.cores' = numCores)
 registerDoParallel(numCores) #specifies that you want 8
@@ -34,13 +36,13 @@ registerDoParallel(numCores) #specifies that you want 8
 
 #test time
 
-system.time(
-    res <- foreach(i = 1:100000) %dopar%
-        {
-            i*i
-
-        }
-)
+# system.time(
+#     res <- foreach(i = 1:100000) %dopar%
+#         {
+#             i*i
+#
+#         }
+# )
 
 #for dopar
 # user  system elapsed
@@ -65,7 +67,13 @@ spe_postqc <-
         )
     )
 
-pAbeta <- as.matrix(colData(spe_postqc)$PAbeta)
+spe_postqc <- scuttle::logNormCounts(spe_postqc)
+
+
+PAbeta <- as.matrix(colData(spe_postqc)$PAbeta)
+PpTau <- as.matrix(colData(spe_postqc)$PpTau)
+
+
 
 #beta = pvec(1:10, corr_for_counts, spe = spe_postqc, mc.cores = numCores)
 #
@@ -74,14 +82,17 @@ pAbeta <- as.matrix(colData(spe_postqc)$PAbeta)
 #             FUN = corr_for_counts)
 
 
-spe_counts <- counts(spe_postqc)
+spe_counts <- logcounts(spe_postqc)
+rm(spe_postqc)
+
+
 t0 = Sys.time()
 res <- foreach(i = 1:27853,
                .combine = rbind,
                .multicombine = TRUE,
                .inorder = FALSE,
                .packages = c('data.table', 'doParallel')) %dopar% {
-                   cor(as.matrix(spe_counts[i,]), pAbeta, method = 'pearson')
+                   cor(as.matrix(spe_counts[i,]), PpTau, method = 'pearson')
                }
 
 t1 = Sys.time()
@@ -96,12 +107,12 @@ difftime(t1, t0, unit="secs")
 
 print(res[1:10])
 
-pAbeta_res <- cbind(rowData(spe_postqc)$gene_id,
+PpTau_res <- cbind(rowData(spe_postqc)$gene_id,
                     res)
-saveRDS(pAbeta_res,
+saveRDS(PpTau_res,
         here("corr_outputs",
              "pearson",
-             "pearson_pAbeta.RDS"))
+             "pearson_PpTau.RDS"))
 
 
 
--- code/01_correlation_exploration/02_explore_corr_values.R
+++ code/01_correlation_exploration/02_explore_corr_values.R
@@ -2,6 +2,7 @@ library(rtracklayer)
 library(GenomicRanges)
 library(SpatialExperiment)
 library(here)
+library(ggplot2)
 library(dplyr)
 library(Matrix)
 # install.packages("DescTools") #was used for converting rho to fisher's Z
@@ -20,25 +21,45 @@ pearson_pAbeta <- as.data.frame(pearson_pAbeta)
 colnames(pearson_pAbeta) <- c("gene_id", "pAbeta_value")
 
 
+
 #in descending order
+pearson_pAbeta$pAbeta_value <- as.numeric(pearson_pAbeta$pAbeta_value)
 pearson_pAbeta <- pearson_pAbeta |> arrange(desc(pAbeta_value))
 pearson_pAbeta
 
 ##make plots to explore correlation values trend pearson pAbeta
 
-dir.create(here("plots",
-           "01_correlation_exploration",
-           "02_explore_corr_values",
-           recursive = TRUE))
+# dir.create(here("plots",
+#            "01_correlation_exploration",
+#            "02_explore_corr_values",
+#            recursive = TRUE))
 output_dir <- here("plots",
                    "01_correlation_exploration",
                    "02_explore_corr_values")
+
+##check last x values
+tail(pearson_pAbeta$pAbeta_value, 600)
+#lot of NAs
+#NAs can appear if there are attributes with zero variance
+#(with all elements equal)
+
+#number of values that are not NA
+sum(!is.na(pearson_pAbeta$pAbeta_value))
+#27349
+#num NAs is 504.
+
+
 # Give the chart file a name.
-pdf(file = output_dir, "pAbeta_pearson_values.pdf")
-
+pdf(file = paste0(output_dir, "/pAbeta_pearson_values.pdf"))
 # Plot the bar chart.
-plot(v,type = "o")
-
+ggplot(data = pearson_pAbeta,
+       aes(x = "", y =pAbeta_value)) +
+    geom_boxplot(fill="slateblue", alpha=0.2) +
+    xlab("") +
+    ylab("Range of pAbeta correlation for each gene")
 # Save the file.
 dev.off()
+
+###
+
 
