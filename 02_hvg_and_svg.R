628b05b251274444b3aafd9cbd3d26ee4641eaf3
Sowmya Parthiban <sparthi1@jh.edu>
2022-07-23 15:03:35 -0400

    remove redundant libraries

--- 02_hvg_and_svg.R
+++ 02_hvg_and_svg.R
@@ -4,9 +4,39 @@ library(GenomicRanges)
 library(BiocParallel)
 library(doParallel)
 library(here)
-devtools::install_github("cellgeni/sceasy")
-library(sceasy)
-library(reticulate)
+library(scran)
+library(nnSVG)
 
-sceasy::convertFormat(spe_postqc, from="sce", to="anndata",
-                      outFile='filename.h5ad')
+spe_postqc <-
+    readRDS(
+        here::here(
+            "input_data",
+            paste0("spe_wholegenome_postqc.rds")
+        )
+    )
+
+spe_postqc <- logNormCounts(spe_postqc)
+
+# fit mean-variance relationship
+dec <- modelGeneVar(spe_postqc)
+
+# visualize mean-variance relationship
+fit <- metadata(dec)
+plot(fit$mean, fit$var,
+     xlab = "mean of log-expression", ylab = "variance of log-expression")
+curve(fit$trend(x), col = "dodgerblue", add = TRUE, lwd = 2)
+
+top_hvgs <- getTopHVGs(dec, prop = 0.1)
+length(top_hvgs)
+
+saveRDS()
+
+####
+
+
+
+
+
+
+
+
