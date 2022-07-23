4702e3008e489293ca642f5af0b9601d8a40db6e
Sowmya Parthiban <sparthi1@jh.edu>
2022-07-23 15:05:08 -0400

    testing out lines for the create_table function

--- code/01_correlation_exploration/02_explore_corr_values.R
+++ code/01_correlation_exploration/02_explore_corr_values.R
@@ -1,16 +1,15 @@
 library(SpatialExperiment)
 library(here)
-install.packages('ggvis')
-library(ggvis)
+library(ggplot2)
 library(dplyr)
 library(sessioninfo)
 # install.packages("DescTools") #was used for converting rho to fisher's Z
 # library(DescTools)
 
-#read in pearson pAbeta correlations
-pearson_PAbeta <- readRDS(here("corr_outputs",
+#### read in pearson pAbeta correlations ####
+pearson_PAbeta <- as.data.frame(readRDS(here("corr_outputs",
                               "pearson",
-                              "pearson_pAbeta.RDS") )
+                              "pearson_pAbeta.RDS") ))
 
 pearson_PpTau<- readRDS(here("corr_outputs",
                                "pearson",
@@ -25,7 +24,7 @@ spearman_PpTau<- readRDS(here("corr_outputs",
                                 "PpTau.RDS") )
 
 
-#convert RDS to dataframe
+##### convert RDS to dataframe ####
 
 
 corr_vals<- as.data.frame(cbind(pearson_PAbeta[,1:2],
@@ -41,18 +40,57 @@ colnames(corr_vals) <- c("gene_id", "pearson_PAbeta",
                          "spearman_PAbeta",
                          "spearman_PpTau")
 
-corr_vals <- corr_vals |> pivot_longer(!gene_id, names_to = "corr_and_path_type",
-                          values_to = "correlation")
-corr_vals$correlation <- as.numeric(corr_vals$correlation)
 
-#
-# #in descending order
-# pearson_pAbeta$pAbeta_value <- as.numeric(pearson_pAbeta$pAbeta_value)
-# pearson_pAbeta <- pearson_pAbeta |> arrange(desc(pAbeta_value))
+#in descending order
+
 # pearson_pAbeta
+### make correlation plots for the top 50 genes.
+names(pearson_PAbeta) <- c("gene_id", "pAbeta_value")
+pearson_PAbeta[,2] <- as.numeric(pearson_PAbeta[,2])
+pearson_PAbeta <- pearson_PAbeta |> arrange(desc(pAbeta_value))
 
-##make plots to explore correlation values trend pearson pAbeta
 
+
+spe_postqc <-
+    readRDS(
+        here::here(
+            "input_data",
+            paste0("spe_wholegenome_postqc.rds")
+        )
+    )
+
+spe_postqc <- logNormCounts(spe_postqc)
+
+known_gene_ids <- pearson_PAbeta$gene_id[1:100]
+ix_known <- which(rowData(spe_postqc)$gene_id %in% known_gene_ids)
+
+spe <- spe_postqc[ix_known, ]
+
+output_dir <- here("plots",
+                   "01_correlation_exploration",
+                   "02_explore_corr_values")
+
+
+pdf(file = paste0(output_dir, "/top_100_genes_pAbeta_pearson.pdf"))
+
+
+for (i in seq_len(100)){
+    par(mfrow=c(1,1))
+    plot(as.vector(logcounts(spe)[i,]), as.vector(colData(spe)$PAbeta),
+         xlab = paste0(rowData(spe)$gene_name[i], " ",
+                       rowData(spe)$gene_id[i]," ", "expression"),
+         ylab = "PAbeta_value")
+
+
+}
+
+dev.off()
+
+
+
+corr_vals_pAbeta <
+corr_vals$pearson_PAbeta <- as.numeric(corr_vals$pearson_PAbeta)
+pearson_pAbeta <-  corr_vals |> arrange(desc(corr_vals$pearson_PAbeta))
 
 ##check last x values
 tail(pearson_pAbeta$pAbeta_value, 600)
@@ -87,7 +125,7 @@ output_dir <- here("plots",
                    "02_explore_corr_values")
 
 
-#make plots and patch them
+##### make plots  #####
 
 
 # Give the chart file a name.
@@ -103,6 +141,16 @@ ggplot(corr_vals, aes(x = corr_and_path_type, y = correlation, fill = corr_and_p
 # Save the file.
 dev.off()
 
-###
+##### List top 100 expressed and depleted genes
+
+top_100 expressed and depleted genes.
+
+
+
+
+
+
+
+
 
 
