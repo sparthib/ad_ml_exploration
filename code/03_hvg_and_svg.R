437f67ac6f724ddca799b42c4012baa9ae711cb1
Sowmya Parthiban <sparthi1@jh.edu>
2022-07-23 15:06:47 -0400

    tried to create a single script for HVGs and SVGs, it's been separated into two scripts now

---
+++ code/03_hvg_and_svg.R
@@ -0,0 +1,37 @@
+library(SpatialExperiment)
+library(rtracklayer)
+library(GenomicRanges)
+library(BiocParallel)
+library(doParallel)
+library(here)
+library(scran)
+library(nnSVG)
+
+spe_postqc <-
+    readRDS(
+        here::here(
+            "input_data",
+            paste0("spe_wholegenome_postqc.rds")
+        )
+    )
+
+spe_postqc <- logNormCounts(spe_postqc)
+
+# fit mean-variance relationship
+dec <- modelGeneVar(spe_postqc)
+
+# visualize mean-variance relationship
+fit <- metadata(dec)
+plot(fit$mean, fit$var,
+     xlab = "mean of log-expression", ylab = "variance of log-expression")
+curve(fit$trend(x), col = "dodgerblue", add = TRUE, lwd = 2)
+
+top_hvgs <- getTopHVGs(dec, prop = 0.1)
+length(top_hvgs)
+
+saveRDS(top_hvgs, 'corr_outputs/top_hvgs.rds')
+
+####
+
+# run nnSVG once per sample-part within LC annotated regions
+# and store lists of top SVGs
