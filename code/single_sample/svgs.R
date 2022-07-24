0d702bfb7ccd7ec1f52069bf74aa351841c14e8d
Sowmya Parthiban <sparthi1@jh.edu>
2022-07-23 20:09:42 -0400

    script for computing svgs borrowed from Lukas

---
+++ code/single_sample/svgs.R
@@ -0,0 +1,64 @@
+library(sgejobs)
+sgejobs::job_single('svgs',
+                    create_shell = TRUE,
+                    create_logdir = FALSE, task_num = 10,
+                    cores = 5L,
+                    queue = "bluejay",
+                    command = "Rscript svgs.R",
+                    memory = "20G")
+
+
+
+library(SpatialExperiment)
+library(doParallel)
+library(here)
+library(dplyr)
+library(scuttle)
+library(sessioninfo)
+library(nnSVG)
+library(BiocParallel)
+library(doParallel)
+here()
+
+#### load spe object and calc log counts ####
+spe_postqc <-
+    readRDS(here::here("input_data",
+                       "spe_wholegenome_postqc.rds"))
+
+print(class(spe_postqc))
+
+#### get task id and subset spe ####
+s = as.numeric(Sys.getenv("SGE_TASK_ID"))
+ix <- colData(spe_postqc)$sample_id_short == sample_ids[s]
+spe_sub <- spe_postqc[, ix]
+
+print(sample_ids[s])
+
+
+# run nnSVG filtering for mitochondrial gene and low-expressed genes
+spe_sub <- filter_genes(
+    spe_sub,
+    filter_genes_ncounts = 2,
+    filter_genes_pcspots = 1
+)
+
+# re-calculate logcounts after filtering
+spe_sub <- logNormCounts(spe_sub)
+
+
+# run nnSVG
+set.seed(123)
+spe_sub <- nnSVG(spe_sub, n_threads = 5)
+
+# store results
+res_list <- rowData(spe_sub)
+
+
+# ------------
+# save results
+# ------------
+
+# save nnSVG results
+output_dir <- here('corr_outputs',
+                   'sample_ids[s]')
+saveRDS(res_list, paste0(output_dir, "/top_svgs.RDS"))
