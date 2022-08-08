817ed65880a2e93cf5188b47d3b3bfd6297a18eb
Sowmya Parthiban <sparthi1@jh.edu>
2022-08-08 10:39:10 -0400

    create shell scripts to non_zero_filtered_genes_pathology

---
+++ code/single_sample/non_zero_filtered_genes_PpTau.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#$ -cwd
+#$ -l bluejay,mem_free=20G,h_vmem=20G,h_fsize=100G
+#$ -pe local 5
+#$ -N non_zero_filtered_genes_PpTau
+#$ -o logs/non_zero_filtered_genes_PpTau.$TASK_ID.txt
+#$ -e logs/non_zero_filtered_genes_PpTau.$TASK_ID.txt
+#$ -m e
+#$ -t 1-10
+#$ -tc 20
+
+echo "**** Job starts ****"
+date
+
+echo "**** JHPCE info ****"
+echo "User: ${USER}"
+echo "Job id: ${JOB_ID}"
+echo "Job name: ${JOB_NAME}"
+echo "Hostname: ${HOSTNAME}"
+echo "Task id: ${SGE_TASK_ID}"
+
+## Load the R module (absent since the JHPCE upgrade to CentOS v7)
+module load conda_R
+
+## List current modules for reproducibility
+module list
+
+## Edit with your job command
+Rscript 04b_non_zero_filtered_genes_PpTau.R
+
+echo "**** Job ends ****"
+date
+
+## This script was made using sgejobs version 0.99.1
+## available from http://research.libd.org/sgejobs/
