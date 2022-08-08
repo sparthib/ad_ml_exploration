693e17d172e0a6e660e0cafcb38e028a6b6bea2a
Sowmya Parthiban <sparthi1@jh.edu>
2022-08-08 10:25:36 -0400

    code for comparing hvgs with svgs

--- code/03_hvg_and_svg.R
+++ code/03_hvg_and_svg.R
@@ -7,6 +7,8 @@ library(here)
 library(scran)
 library(nnSVG)
 
+
+
 spe_postqc <-
     readRDS(
         here::here(
@@ -17,17 +19,43 @@ spe_postqc <-
 
 spe_postqc <- logNormCounts(spe_postqc)
 
-# fit mean-variance relationship
+
+####sample id names ####
+sample_ids <- c(
+    "S1_A1_Br3874" ,
+    "S1_B1_Br3854",
+    "S1_C1_Br3873" ,
+    "S1_D1_Br3880" ,
+    "S2_A1_Br3874" ,
+    "S2_B1_Br3854",
+    "S2_C1_Br3873",
+    "S2_D1_Br3880",
+    "S3_A1_Br3874" ,
+    "S3_D1_Br3880"
+)
+print("sample id names listed")
+
+
+#### get task id and subset spe ####
+s = as.numeric(Sys.getenv("SGE_TASK_ID"))
+ix <- colData(spe_postqc)$sample_id_short == sample_ids[s]
+spe_sub <- spe_postqc[, ix]
+
+print(sample_ids[s])
+
+
+##### fit mean-variance relationship ####
 dec <- modelGeneVar(spe_postqc)
 
 # visualize mean-variance relationship
-fit <- metadata(dec)
-plot(fit$mean, fit$var,
-     xlab = "mean of log-expression", ylab = "variance of log-expression")
-curve(fit$trend(x), col = "dodgerblue", add = TRUE, lwd = 2)
+# fit <- metadata(dec)
+# plot(fit$mean, fit$var,
+#      xlab = "mean of log-expression", ylab = "variance of log-expression")
+# curve(fit$trend(x), col = "dodgerblue", add = TRUE, lwd = 2)
 
 top_hvgs <- getTopHVGs(dec, prop = 0.1)
 length(top_hvgs)
+
 
 saveRDS(top_hvgs, 'corr_outputs/top_hvgs.rds')
 
